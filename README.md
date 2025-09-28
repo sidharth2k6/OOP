2cac6a8418bb9f64a693dc7810de9e9089de3afd
Sidharth <sidharthacker@gmail.com>
2025-09-28 13:14:26 +0530

    question no 2 solve ; ;

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 <br>
 Q1. Write a program that store 10 nmbers in the array. The user should be able to search a particular number in the array. Implement sequential search for this purpose.
 <br>
+Q2. Implement the above program for binary search. Please note that binary search works with the sorted arrays only.
+<br>
---
+++ binarysearch.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[10], temp;
+    int i, j, flag = 0;
+    int start, end;
+    int search, mid;
+
+    cout << "Plese enter ten array elements" << endl;
+    for(i = 0; i < 10; i++){
+        cin >> a[i];
+    }
+    cout << "Plese enter the number to be searched: ";
+    cin >> search;
+
+    //Array Sorting
+    for(j = 0; j < 9; j++){
+        for(i = 0; i < 9; i++){
+            if(a[i] > a[i + 1]){
+                temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+    }
+
+    // for(i = 0; i < 10; i++){
+    //     cout << a[i] << " ";
+    // }
+
+    //Searching
+    start = 0;
+    end = 9;
+
+    do{
+        mid = (start + end)/2;
+        if(a[mid] == search){
+            cout << search << " found.";
+            flag = 1;
+            break;
+        }
+        if(a[mid] > search){
+            end = mid;
+        }
+        else if(a[mid] < search){
+            start = mid;
+        }
+    }while(start < end && mid != start);
+
+    if(flag == 0){
+        cout << search << " not found!";
+    }
+    return 0;
+}
